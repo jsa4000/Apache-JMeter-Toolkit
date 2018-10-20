508fe941e62f9d955056f8fb86af899b37821809
jsa4000 <jsa4000@gmail.com>
2018-10-20 20:20:46 +0200

    Keep with the modifications

--- files/examples/test-script.sh
+++ files/examples/test-script.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-echo "##################################"
+echo "----------------------------------" 
 echo "--    Running JMeter Script     --" 
-
+echo "----------------------------------" 
+echo ""
 echo "----------------------------------" 
 echo "  Test 01" 
 echo "----------------------------------" 
+echo ""
 jmeter-start.sh -t  Apache-JMeter-Toolkit/files/examples/Test01.linux.jmx -l file-output-01.csv -j test-01.log
-
+echo ""
 echo "----------------------------------" 
 echo "  Test 02" 
 echo "----------------------------------" 
+echo ""
 jmeter-start.sh -t  Apache-JMeter-Toolkit/files/examples/Test01.linux.jmx -l file-output-02.csv -j test-02.log
-
+echo ""
 echo "----------------------------------"
 echo "  Test 03" 
 echo "----------------------------------"
+echo ""
 jmeter-start.sh -t  Apache-JMeter-Toolkit/files/examples/Test01.linux.jmx -l file-output-03.csv -j test-03.log
-
+echo ""
 echo "----------------------------------"
 echo "  Tests Finished" 
-echo "##################################"
+echo "----------------------------------" 
