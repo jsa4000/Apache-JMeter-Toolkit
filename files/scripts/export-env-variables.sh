59dd022cac7791f83a3a2e08dc0277343eedf8a1
jsa4000 <jsa4000@gmail.com>
2018-10-28 23:11:31 +0100

    Fixed the exportation of the environment variables

--- files/scripts/export-env-variables.sh
+++ files/scripts/export-env-variables.sh
@@ -1,23 +1,31 @@
-export JMETER_SOURCE /mnt/source
-export JMETER_OUTPUT_PATH /tmp/jmeter
-export JMETER_SERVER_ID 0
-export JMETER_CLIENT_PORT 7000
-export JMETER_SERVER_PORT 60000
-export JMETER_SERVER_HOSTNAME NONE
-export JMETER_RMI_SSL_DISABLED TRUE
-export JMETER_SERVER_ENABLED FALSE
-export JMETER_REMOTE_SERVERS NONE
-export JMETER_CLOSE_REMOTE_SERVERS FALSE
-export JMETER_SCRIPT_MODE FALSE
-export JMETER_AUTOMATIC_OUTPUT_ENABLED TRUE
-export JMETER_PARAM_TYPE J
 
-export JMETER_SUMMARY_FILENAME summary.csv
-export JMETER_LOG_FILENAME logfile.log
-export JMETER_DASHBOARD_PATHNAME dashboard
-export JMETER_COMPRESSED_FILENAME summary-outputs.tar.gz
+# JMeter Global variables
+export JMETER_SOURCE=/mnt/source
+export JMETER_OUTPUT_PATH=/tmp/jmeter
 
-export JMETER_EXTRA_PARAMETERS NONE
-export MINIO_UPLOAD_ENABLED FALSE
-export MINIO_BUCKET_NAME NONE
-export MC_HOSTS_REPO https://access-key:secret-key@s3-endpoint
+# JMeter Server and Client variables
+export JMETER_SERVER_ID=0
+export JMETER_CLIENT_PORT=7000
+export JMETER_SERVER_PORT=60000
+export JMETER_SERVER_HOSTNAME=NONE
+export JMETER_RMI_SSL_DISABLED=TRUE
+export JMETER_SERVER_ENABLED=FALSE
+export JMETER_REMOTE_SERVERS=NONE
+export JMETER_CLOSE_REMOTE_SERVERS=FALSE
+export JMETER_SCRIPT_MODE=FALSE
+export JMETER_AUTOMATIC_OUTPUT_ENABLED=TRUE
+export JMETER_PARAM_TYPE=J
+
+# JMeter Output names
+export JMETER_SUMMARY_FILENAME=summary.csv
+export JMETER_LOG_FILENAME=logfile.log
+export JMETER_DASHBOARD_PATHNAM=dashboard
+export JMETER_COMPRESSED_FILENAME=summary-outputs.tar.gz
+
+# Extra parameters not used. It can be exported any environment variable to the container.
+export JMETER_EXTRA_PARAMETERS=NONE
+
+# Minio specific variables
+export MINIO_UPLOAD_ENABLED=FALSE
+export MINIO_BUCKET_NAME=NONE
+export MC_HOSTS_REPO=https://access-key:secret-key@s3-endpoint
